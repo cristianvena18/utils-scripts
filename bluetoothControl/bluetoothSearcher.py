23d29c76001ad49a82be1bab5b99eabbd8f6fec2
cristianvena18 <cristiandamianvena@gmail.com>
2020-10-05 20:11:57 -0300

    build: add bluetooth control files

---
+++ bluetoothControl/bluetoothSearcher.py
@@ -0,0 +1,13 @@
+import bluetooth
+
+print("Performing inquiry...")
+
+nearby_devices = bluetooth.discover_devices(lookup_names=True)
+
+print("Found {} devices".format(len(nearby_devices)))
+
+for addr, name in nearby_devices:
+    try:
+        print("   {} - {}".format(addr, name))
+    except UnicodeEncodeError:
+        print("   {} - {}".format(addr, name.encode("utf-8", "replace")))
